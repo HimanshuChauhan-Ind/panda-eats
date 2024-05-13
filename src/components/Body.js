92f742bb482d9bf5202c19fee40ed12960b03b0d
Himanshu Chauhan <him97cha@gmail.com>
2024-05-13 12:38:57 +0530

    Filter Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import Card from "./Card";
 import mockData from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [resData, setResData] = useState(mockData);
+
   return (
     <div className="res-body">
-      <div className="search">search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredData = resData.filter(
+              (item) => item.info.avgRating > 4
+            );
+            setResData(filteredData);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {mockData.map((item) => (
+        {resData.map((item) => (
           <Card key={item.info.id} resData={item} />
         ))}
       </div>
