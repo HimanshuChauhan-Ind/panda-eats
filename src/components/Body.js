5808c56cc2896f86c0674c84dcd87e78fece33a2
Himanshu Chauhan <him97cha@gmail.com>
2024-05-14 11:47:39 +0530

    Added the live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,23 @@
 import Card from "./Card";
 import mockData from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [resData, setResData] = useState(mockData);
+  const [resData, setResData] = useState([]);
+
+  useEffect(() => fetchData(), []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.61450&lng=77.30630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    setResData(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
 
   return (
     <div className="res-body">
